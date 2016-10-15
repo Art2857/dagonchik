9a83bc540d86d61c6ef7cd894393b52ff6dc8258
Art2857 <qwertzom@yandex.ru>
2016-10-16 03:11:34 +0800

    Add files via upload

--- Dagon_Stealer/Program.cs
+++ Dagon_Stealer/Program.cs
@@ -269,10 +269,10 @@ namespace Dagon_Stealer
                                 if (nmf > 1)
                                 {
                                     meepo[maxhpfwmeepo].Stop();
-                                    poof[maxhpfwmeepo] = minhpmeepo; Utils.Sleep(100, "pf" + maxhpfwmeepo.ToString()); //meepo[maxhpfwmeepo].Spellbook.SpellW.UseAbility(meepo[minhpmeepo].Position);
+                                    poof[maxhpfwmeepo] = minhpmeepo; Utils.Sleep(1, "pf" + maxhpfwmeepo.ToString()); //meepo[maxhpfwmeepo].Spellbook.SpellW.UseAbility(meepo[minhpmeepo].Position);
                                 }
                                 meepo[minhpmeepo].Stop();
-                                poof[minhpmeepo] = j; Utils.Sleep(100, "pf" + minhpmeepo.ToString());//meepo[minhpmeepo].Spellbook.SpellW.UseAbility(meepo[j].Position);
+                                poof[minhpmeepo] = j; Utils.Sleep(250, "pf" + minhpmeepo.ToString());//meepo[minhpmeepo].Spellbook.SpellW.UseAbility(meepo[j].Position);
                             }
 
                         
