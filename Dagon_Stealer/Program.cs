737d69c4e401a341539f9fce86868b2e67c1e04c
Art2857 <qwertzom@yandex.ru>
2016-10-17 21:18:36 +0800

    Add files via upload

--- Dagon_Stealer/Program.cs
+++ Dagon_Stealer/Program.cs
@@ -506,7 +506,7 @@ namespace Dagon_Stealer
                                 nmui += 1;
                                 switch (i1)
                                 {
-                                    case 0: { if (discord != null) { if (dist < 1600 /*|| bd==true*/) { dmg *= 1.25; } } break; }
+                                    case 0: { if (discord != null) { if (dist < 1600 || bd == true) { dmg = dmg * 1.25; } } break; }
                                     case 1: { /*if (ethereal != null) { if (dist < ethereal.CastRange() || bd) { damag += ethereal_damag * dmg; dmg *= 1.4; }}*/ break;  }
                                     case 2: { /*if (dagon != null) { if (dist < dagon.CastRange() || bd) { damag += (400 + 100 * (dagon.Level - 1)) * dmg; } }*/ break; }
                                     case 3: { /*if ((orchid != null || bloodthorn != null)) { if (dist < 900 || bd) { dmg *= 1.3; } }*/ break; }
